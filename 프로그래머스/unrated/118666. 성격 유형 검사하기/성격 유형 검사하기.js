b9941620a1d40c0be69928c1d6622bf2f9a64904
MA <36831218+rmaomina@users.noreply.github.com>
2022-08-23 23:56:35 +0900

    [unrated] Title: 성격 유형 검사하기, Time: 0.77 ms, Memory: 30.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/118666. 성격 유형 검사하기/성격 유형 검사하기.js
@@ -0,0 +1,37 @@
+function solution(survey, choices) {
+    
+  // 질문마다 판단하는 지표를 담은 1차원 문자열 배열 survey와 검사자가 
+  // 각 질문마다 선택한 선택지를 담은 1차원 정수 배열 choices가 매개변수로 주어짐.
+  // 검사자의 성격 유형 검사 결과를 지표 번호 순서대로 return. 
+  // 
+  let result = '';
+  let record = { 'R': 0, 'T': 0, 'C': 0, 'F': 0, 'J': 0, 'M': 0, 'A': 0, 'N': 0 }
+  let types = ["RT", "CF", "JM", "AN"]
+  // survey와 choices의 길이는 같다. 
+  // 객체에 각 유형에 대한 점수를 기록하고
+  // 더 큰 점수를 받은 유형을 반환하되, 
+  // 동일하거나 없으면 알파멧 순으로 출력 
+
+  // 저장된 기록에서 R과 T 비교, C와 F와 비교.. 반복 ..
+  const compare = (a, b) => {
+    if(record[a] === record[b]) return [a, b].sort()[0]
+
+    if(record[a] > record[b]) {
+      return a
+    } else {
+      return b
+    }
+  }
+
+  survey.map((elem, idx) => { // elem === "TR"
+    if (choices[idx] < 4) {
+      record[elem[0]] += (4 - choices[idx]) // 1, 2, 3 이면 각각 3, 2, 1 점을 얻는다. 
+    } else if (choices[idx] > 4) {
+      record[elem[1]] += (choices[idx] - 4) // 5, 6, 7 이면 각각 1, 2, 3 점을 얻는다. 
+    }
+  })
+
+  types.forEach(el => result += compare(el[0], el[1]))
+
+  return result;
+}
