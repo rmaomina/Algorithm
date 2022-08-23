6c85e9f76f35ced90e7eaa173a68594545e78ad5
mina <rmaomina@gmail.com>
2022-08-23 21:47:23 +0900

    완주하지 못한 선수 풀이 과정 추가

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,4 +1,42 @@
 function solution(participant, completion) {
+  // 마라톤에 참여한 선수들의 이름이 담긴 배열 participant와 
+  // 완주한 선수들의 이름이 담긴 배열 completion이 주어질 때, 
+  // 완주하지 못한 선수의 이름을 return
+
+  // ----------------- 첫번째 -------------------
+  // splice를 사용하고 효율성 통과가 되지 않음. 효율성 문제인 것 같다. 
+  // [Done] exited with code=0 in 0.084 seconds
+
+  // completion.forEach(person => {
+  //   participant.splice(participant.indexOf(person), 1);
+  // });
+  
+  // return participant[0];
+
+  // ------------------ 두번째 ----------------------
+  
+  // participant가 완주했는지 기록한다. 
+  // let record = {}
+  // for (let person of participant) {
+  //   // 동명이인 처리가 안됨 --> 완주한 사람이 몇 명인지 확인 
+  //   if (!record[person]) {
+  //     record[person] = 1
+  //   } else {
+  //     record[person] += 1
+  //   }
+  // }
+  // console.log(record)
+
+  // completion.forEach(elem => {
+  //   if (record[elem]) record[elem] -= 1
+  // });
+  
+  // return Object.keys(record).filter(x => record[x] === 1)[0]
+
+  // ---------------- 세번째 ---------------------
+
+  // 힌트를 보니 순서를 정렬하고 인덱스를 비교해서 인덱스와 다르면 바로 return 하는 것 같음. 
+  // [Done] exited with code=0 in 0.075 seconds
   participant = participant.sort()
   completion = completion.sort()
 
@@ -15,3 +53,6 @@ function solution(participant, completion) {
   }
   return result
 }
+
+console.log(solution(["leo", "kiki", "eden"], ["eden", "kiki"])) // --> 'leo'
+console.log(solution(["leo", "kiki", "eden", "eden"], ["eden", "kiki", "leo"])) // --> 'eden'
