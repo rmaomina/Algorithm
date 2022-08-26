aa267bb7005330edefe7661e8d14ecd9a55419c5
mina <rmaomina@gmail.com>
2022-08-26 13:24:38 +0900

    '소수 찾기' 복기 & 정렬 수정

--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.js
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.js
@@ -1,21 +1,16 @@
 function solution(n) {
   let prime = 0
 
-  
-  
   for (let i = 2; i <= n; i++) { // 10이라고 하면 2부터 10까지 소수인지 확인해야 함. 
-    if (isPrimeNum(i)) prime++
+    if (isPrimeNum(i)) prime += 1
   }
 
   return prime;
 }
 
 function isPrimeNum(num) { // 2, 3 ... 10
-  
     // 2의 배수는 소수가 아니다. (단 2를 제외하고) 6
-    if (num % 2 === 0 && num !== 2) {
-        return false
-    }
+    if (num !== 2 && num % 2 === 0) return false
 
     let sqrt = Math.floor(Math.sqrt(num))
     // 이미 계산한 값은 중복해서 하지 말자. 3일 떄, 3 (소수) / 11일 때, 3, 5, 7, 9, 11
