64b792cc85ad10f1e90e333a3c1df7317b03c803
mina <rmaomina@gmail.com>
2022-08-26 13:24:09 +0900

    '없는 숫자 더하기' 다른 사람 풀이 추가

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -1,8 +1,14 @@
 function solution(numbers) {
     let answer = 0;
     
-    for (let i = 0; i < 10; i++){
-        if (numbers.indexOf(i) === -1) answer += i; 
-    } 
-    return answer;
+    // for (let i = 0; i < 10; i++){
+    //     if (numbers.indexOf(i) === -1) answer += i; 
+    // } 
+    // return answer;
+
+    // 다른 사람 풀이
+    return 45 - numbers.reduce((acc, cur) => acc + cur)
 }
+
+console.log((solution([1,2,3])))
+console.log((solution([9,7,5,1,2])))
