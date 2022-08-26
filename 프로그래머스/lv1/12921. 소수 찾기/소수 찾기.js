934a598fe4a8c620062dba21d5c6498561d322c8
mina <rmaomina@gmail.com>
2022-08-26 13:28:50 +0900

    '소수 찾기' 테스트 케이스 추가

--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.js
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.js
@@ -9,15 +9,18 @@ function solution(n) {
 }
 
 function isPrimeNum(num) { // 2, 3 ... 10
-    // 2의 배수는 소수가 아니다. (단 2를 제외하고) 6
-    if (num !== 2 && num % 2 === 0) return false
+  // 2의 배수는 소수가 아니다. (단 2를 제외하고) 6
+  if (num !== 2 && num % 2 === 0) return false
 
-    let sqrt = Math.floor(Math.sqrt(num))
-    // 이미 계산한 값은 중복해서 하지 말자. 3일 떄, 3 (소수) / 11일 때, 3, 5, 7, 9, 11
-    for (let n = 3; n <= sqrt; n += 2) { // 3부터 홀수만
-      if (num % n === 0) {
-        return false
-      }
+  let sqrt = Math.floor(Math.sqrt(num))
+  // 이미 계산한 값은 중복해서 하지 말자. 3일 떄, 3 (소수) / 11일 때, 3, 5, 7, 9, 11
+  for (let n = 3; n <= sqrt; n += 2) { // 3부터 홀수만
+    if (num % n === 0) {
+      return false
     }
-    return true
   }
+  return true
+}
+
+console.log(solution(17)) // --> 2, 3, 5, 7, 11, 13, 17 (7)
+console.log(solution(778)) // --> 
