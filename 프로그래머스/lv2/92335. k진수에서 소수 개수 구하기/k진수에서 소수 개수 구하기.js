f988b80942af8d498e1f7d19583b3267afd80cda
mina <rmaomina@gmail.com>
2022-09-03 02:40:36 +0900

    'k진수에서 소수' 테스트 케이스 추가

--- 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.js
+++ 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.js
@@ -1,17 +1,21 @@
+
+// 숫자 n은 1 <= n <= 1,000,000
+// 진수 k는 3 <= k <= 10 
+
 function solution(n, k) {
-    let answer = 0
-    let baseArr = n.toString(k).split('0').filter(el => el !== '')
-    let primeArr = baseArr.filter(el => isPrime(Number(el)))
+  let baseArr = n.toString(k).split('0').filter(el => el !== '')
+  let primeArr = baseArr.filter(el => isPrime(Number(el)))
 
-    console.log(baseArr)
-    console.log(primeArr)
-    return primeArr.length;
+  return primeArr.length;
 }
-
 
 function isPrime(num) {
-  for (let i = 2; i <= Math.sqrt(num); i++) {
-    if (num % i === 0) return false;
-  }
-  return num >= 2;
+for (let i = 2; i <= Math.sqrt(num); i++) {
+  if (num % i === 0) return false;
 }
+return num >= 2;
+}
+
+
+console.log(solution(437674, 3))
+console.log(solution(110011, 10))
