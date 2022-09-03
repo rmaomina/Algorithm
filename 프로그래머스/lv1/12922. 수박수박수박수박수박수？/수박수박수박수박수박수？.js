2193aab1ac607642d84f73101cc35178379dab8e
MA <36831218+rmaomina@users.noreply.github.com>
2022-09-03 14:07:46 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -4,7 +4,6 @@ function solution(n) {
     let repeat = Math.ceil(n / PATTERN.length)
     
     for (let i = 0; i < repeat; i++) answer += PATTERN
-    console.log(typeof answer)
-    console.log(typeof repeat)
+
     return answer.substring(0, n);
 }
