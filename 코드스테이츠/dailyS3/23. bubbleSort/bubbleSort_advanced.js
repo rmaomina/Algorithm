fe1fe89d2ff9635a4adc889227f93c9697412adb
mina <rmaomina@gmail.com>
2022-09-05 00:59:35 +0900

    코플릿 bubbleSort & advanced

--- 코드스테이츠/dailyS3/23. bubbleSort/bubbleSort_advanced.js
+++ 코드스테이츠/dailyS3/23. bubbleSort/bubbleSort_advanced.js
@@ -1,28 +1,26 @@
 const swap = (idx1, idx2, arr) => {
-  // 두 변수를 바꾸는 방법 
-  // 1. 내가 사용한 방법 임시 교체 
-  // temp = arr[idx1]
-  // arr[idx1] = arr[idx2]
-  // arr[idx2] = temp
-
-  // 2. 구조분해할당을 활용 (참조형이라 가능)
-  [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]]
-
   // 3. XOR 연산을 활용 (참조형이라 가능)
-  // arr[idx1] ^= arr[idx2]
-  // arr[idx2] ^= arr[idx1]
-  // arr[idx1] ^= arr[idx2]
+  arr[idx1] ^= arr[idx2]
+  arr[idx2] ^= arr[idx1]
+  arr[idx1] ^= arr[idx2]
 }
 
 const bubbleSort = function (arr) {
   // TODO: 여기에 코드를 작성합니다.
 
   for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr.length - 1; j++) {
+    // 어떤 요소도 swap되지 않았다면, 이미 배열은 정렬된 것이다. 
+    let swaps = false;
+
+    // 매 반복마다 i번째 요소가 가장 큰
+    // i가 2일 때(두번째 반복할 때), 이미 정렬된 i번째 배열은 검사할 필요가 없다. 
+    for (let j = 0; j < arr.length - 1 - i; j++) {
       if (arr[j] > arr[j + 1]) {
+        swaps = true
         swap(j, j+1, arr)
       }
     }
+    if (!swaps) break
   }
 
   return arr
