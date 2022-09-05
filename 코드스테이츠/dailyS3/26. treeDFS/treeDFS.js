52befd55e7e78e15354bf0609154783d1a48b1f2
mina <rmaomina@gmail.com>
2022-09-05 10:54:35 +0900

    코플릿 treeDFS

--- 코드스테이츠/dailyS3/26. treeDFS/treeDFS.js
+++ 코드스테이츠/dailyS3/26. treeDFS/treeDFS.js
@@ -1,5 +1,14 @@
 let dfs = function (node) {
   // TODO: 여기에 코드를 작성합니다.
+  let values = [node.value] // [1][2][4][6][5][3][7]
+  console.log(values)
+
+  // node의 children이 있을 경우에만 반복. 
+  node.children.forEach(n => { // 2, 4, 6, 5, 3, 7
+    values = values.concat(dfs(n)) // concat([1][2][4][6][5][3][7])
+  })
+
+  return values
 };
 
 // 이 아래 코드는 변경하지 않아도 됩니다. 자유롭게 참고하세요.
@@ -14,3 +23,16 @@ Node.prototype.addChild = function (child) {
   this.children.push(child);
   return child;
 };
+
+let root = new Node(1);
+let rootChild1 = root.addChild(new Node(2));
+let rootChild2 = root.addChild(new Node(3));
+let leaf1 = rootChild1.addChild(new Node(4));
+let leaf2 = rootChild1.addChild(new Node(5));
+let output = dfs(root);
+console.log(output); // --> [1, 2, 4, 5, 3]
+
+leaf1.addChild(new Node(6));
+rootChild2.addChild(new Node(7));
+output = dfs(root);
+console.log(output); // --> [1, 2, 4, 6, 5, 3, 7]
