b2999645592ba18c5a03accf05d928c842e5b471
MA <36831218+rmaomina@users.noreply.github.com>
2022-09-20 23:29:10 +0900

    [level 2] Title: 하노이의 탑, Time: 6.69 ms, Memory: 49 MB -BaekjoonHub

--- 프로그래머스/lv2/12946. 하노이의 탑/하노이의 탑.js
+++ 프로그래머스/lv2/12946. 하노이의 탑/하노이의 탑.js
@@ -1,14 +1,4 @@
 function solution(n) {
-    
-    let result = []
-    const hanoi = (num, from, to, other) => {
-        if (num === 0) return 
-
-        hanoi(num - 1, from, other, to)
-        result.push([from, to])
-        hanoi(num - 1, other, to, from )
-        return result
-    }
-
-    return hanoi(n, 1, 3, 2)
+    var answer = [[]];
+    return answer;
 }
