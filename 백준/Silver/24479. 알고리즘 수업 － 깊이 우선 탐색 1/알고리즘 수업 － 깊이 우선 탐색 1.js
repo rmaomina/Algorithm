45b3ca643e136aaa124f26e151fcfbf98297ecab
mina <rmaomina@gmail.com>
2022-09-21 20:48:59 +0900

    'dfs 깊이 우선 탐색'  코드 이해 및 정리 (실패 코드 추가)

--- 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.js
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.js
@@ -4,7 +4,7 @@ const input = require('fs')
 .trim()
 .split('\n')
 
-const [N, M, R] = input[0].split(" ").map((i) => Number(i))
+const [nodeCount, edgeCount, rootNode] = input[0].split(' ').map((i) => Number(i))
 const edge = input.slice(1).map((e) => e.split(' ').map(Number))
 
 const graph = new Map()
@@ -16,20 +16,58 @@ edge.forEach(([from, to]) => {
 })
 
 let count = 1
-const visited = new Array(N + 1)
-const answer = new Array(N).fill(0)
+const visited = new Array(nodeCount + 1)
+const answer = new Array(nodeCount).fill(0)
 
-const dfs = ((R) => {
-    visited[R] = true
-    answer[R - 1] = count++
-    if (!graph.has(R)) return
-    const nodes = [...graph.get(R)].sort((a, b) => a - b)
-    for (node of nodes) {
+const dfs = ((rootNode) => {
+    visited[rootNode] = true
+    answer[rootNode - 1] = count++
+    if (!graph.has(rootNode)) return
+    const nodes = [...graph.get(rootNode)].sort((a, b) => a - b)
+    for (let node of nodes) {
         if (!visited[node]) {
             dfs(node)
         }
     }
 })
 
-dfs(R)
-console.log(answer.join("\n"))
+dfs(rootNode)
+console.log(answer.join('\n'))
+
+
+// 첫번째 시도 왜 DFS 런타임 에러???
+// const input = require('fs')
+// 	.readFileSync(__dirname + '/input_boj.txt')
+// 	.toString()
+// 	.trim()
+// 	.split('\n')
+// // N, M, R
+
+// let [first, ...rest] = input
+// const [nodeCount, edgeCount, rootNode] = first.split(' ').map(Number)
+// const edge = rest.map((e) => e.split(' ').map(Number))
+
+// const graph = new Map() // {}
+// edge.forEach(([from, to]) => {
+// 	if (graph.has(from)) graph.get(from).push(to)
+// 	else graph.set(from, [to])
+// 	if (graph.has(to)) graph.get(to).push(from)
+// 	else graph.set(to, [from])
+// })
+
+// let count = 1
+// const visited = new Array(nodeCount + 1) // 노드 갯수 + 1 -> 루트노드가 1이라서 
+// const answer = new Array(nodeCount).fill(0)
+
+// const dfs = (rootNode) => {
+// 	visited[rootNode] = true
+// 	answer[rootNode - 1] = count++
+// 	if (!graph.has(rootNode)) return
+// 	const nodes = [...graph.get(rootNode)].sort((a, b) => a - b)
+// 	for (let node of nodes) {
+// 		if (!visited[node]) dfs(node)
+// 	}
+// }
+
+// dfs(rootNode)
+// console.log(answer.join('\n'))
