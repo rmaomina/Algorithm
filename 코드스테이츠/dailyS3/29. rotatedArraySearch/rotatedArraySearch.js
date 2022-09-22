63bbf1cb51882f14a19c40d6f3f453b522243234
mina <rmaomina@gmail.com>
2022-09-23 07:23:52 +0900

    '코플릿' 문제 추가 레퍼런스 참고

--- 코드스테이츠/dailyS3/29. rotatedArraySearch/rotatedArraySearch.js
+++ 코드스테이츠/dailyS3/29. rotatedArraySearch/rotatedArraySearch.js
@@ -1,3 +1,39 @@
+// naive solution
+// const rotatedArraySearch = (rotated, target) => {
+//   for (let i = 0; i < rotated.length; i++) {
+//     if (rotated[i] === target) {
+//       return i;
+//     }
+//   }
+//   return -1;
+// };
+
 const rotatedArraySearch = function (rotated, target) {
-  // TODO : 여기에 코드를 작성합니다.
+  let left = 0
+  let right = rotated.length - 1;
+
+  while (left <= right) {
+    let middle = parseInt((right + left) / 2);
+
+    if (rotated[middle] === target) {
+      return middle;
+    }
+
+    if (rotated[left] < rotated[middle]) {
+      // 왼쪽 절반이 정렬되어 있는 상태
+      if (target < rotated[middle] && rotated[left] <= target) {
+        right = middle - 1;
+      } else {
+        left = middle + 1;
+      }
+    } else {
+      // 오른쪽 절반이 정렬되어 있는 상태
+      if (target <= rotated[right] && rotated[middle] < target) {
+        left = middle + 1;
+      } else {
+        right = middle - 1;
+      }
+    }
+  }
+  return -1;
 };
