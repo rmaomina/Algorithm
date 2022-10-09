239cf4574b99f446fe3f408893ac9ebde3556884
mina <rmaomina@gmail.com>
2022-10-10 05:06:25 +0900

    백준 주유소 레퍼런스 추가

--- 백준/Silver/13305. 주유소/주유소.js
+++ 백준/Silver/13305. 주유소/주유소.js
@@ -1,25 +1,41 @@
-const fs = require("fs");
-const readFileSyncAddress = '/dev/stdin';
+// 주유소 그리디 (내채점: 58점 -> 100점)
+// 차이점은?? BigInt, 숫자 0n & String()
+const path = __dirname + '/input_boj1.txt'
+// const path = '/dev/stdin'
 
-let input = fs.readFileSync(readFileSyncAddress).toString().trim().split("\n");
+const input = require('fs').readFileSync(path).toString().trim().split('\n')
+const dist = input[1].split(' ').map((i) => BigInt(i))
+const cost = input[2].split(' ').map((i) => BigInt(i))
 
-// 인풋 처리
-[n, distances, cities] = input;
-n = Number(n);
-distances = distances.split(' ').map((i) => BigInt(i));
-cities = cities.split(' ').map((i) => BigInt(i));
-solution(n, distances, cities);
+solution(dist, cost)
 
-// 문제 풀이
-function solution(n, distances, cities) {
-	let answer = 0n;
-	let nowOliPrice = cities[0];
+function solution(dist, cost) {
+	let cheapest = cost[0]
+	let totalPrice = 0n
 
-	for (let i = 0; i < distances.length; i += 1) {
-		answer += nowOliPrice * distances[i];
-
-		if (nowOliPrice > cities[i + 1]) nowOliPrice = cities[i + 1];
+	for (let i = 0; i < dist.length; i++) {
+		totalPrice += cheapest * dist[i]
+		if (cheapest > cost[i + 1]) cheapest = cost[i + 1]
 	}
 
-	console.log(String(answer));
+	console.log(String(totalPrice))
 }
+
+// 레퍼런스 (채점: 100점)
+
+// distances = distances.split(' ').map((i) => BigInt(i));
+// cities = cities.split(' ').map((i) => BigInt(i));
+// solution(distances, cities);
+
+// function solution(distances, cities) {
+// 	let answer = 0n;
+// 	let nowOliPrice = cities[0];
+
+// 	for (let i = 0; i < distances.length; i += 1) {
+// 		answer += nowOliPrice * distances[i];
+
+// 		if (nowOliPrice > cities[i + 1]) nowOliPrice = cities[i + 1];
+// 	}
+
+// 	console.log(String(answer));
+// }
