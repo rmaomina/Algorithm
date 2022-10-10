5086a716a99996b5fb0a503268d68ac2b621b261
mina <rmaomina@gmail.com>
2022-10-10 20:41:04 +0900

    백준 '최소 힙' 테스트케이스 추가

--- 백준/Silver/1927. 최소 힙/최소 힙.js
+++ 백준/Silver/1927. 최소 힙/최소 힙.js
@@ -1,5 +1,5 @@
-// const path = __dirname + '/input_boj1.txt'
-const path = '/dev/stdin'
+const path = __dirname + '/input_boj1.txt'
+// const path = '/dev/stdin'
 
 const input = require('fs').readFileSync(path).toString().trim().split('\n')
 
