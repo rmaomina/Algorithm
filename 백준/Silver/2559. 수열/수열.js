30b56f962b346f5a0c0786b238a0ac2e928dbdfa
MA <36831218+rmaomina@users.noreply.github.com>
2022-10-11 17:18:20 +0900

    [Silver III] Title: 수열, Time: 2592 ms, Memory: 17824 KB -BaekjoonHub

---
+++ 백준/Silver/2559. 수열/수열.js
@@ -0,0 +1,25 @@
+// 수열 연속적인 k일의 최대 온도합 구하기 
+// const path = __dirname + '/input_boj1.txt'
+const path = '/dev/stdin'
+
+const input = require('fs').readFileSync(path).toString().trim().split('\n')
+
+let [ N, K ] = input[0].split(' ').map(Number)
+let tempArr = input[1].split(' ').map(Number)
+
+function solution(n, k, array) {
+  let total = []
+  // N이 10, K가 2일 때 9번 반복 10 - 2 + 1 
+  // N이 10, K가 5일 때 6번 반복 10 - 5 + 1
+  for (let i = 0; i < n - (k - 1); i++) { 
+    let tempPerDay = 0
+    for (let j = i; j < i + k; j++) {
+      tempPerDay += array[j]
+    }
+    total.push(tempPerDay)
+  }
+  return total
+}
+
+let result = solution(N, K, tempArr)
+console.log(Math.max(...result))
