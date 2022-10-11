885775f8f27f2059ba8eaed445a6957660877610
mina <rmaomina@gmail.com>
2022-10-11 22:43:21 +0900

    백준 '소수찾기' 에라토스테네스 방식 시도 및 풀이 추가

--- 백준/Silver/1978. 소수 찾기/소수 찾기.js
+++ 백준/Silver/1978. 소수 찾기/소수 찾기.js
@@ -1,8 +1,13 @@
 
-// const path = __dirname + '/input_boj.txt'
-const path = '/dev/stdin'
+const path = __dirname + '/input_boj.txt'
+// const path = '/dev/stdin'
+
+// 에라토스테네스의 체를 사용해 보려고 했지만, 
+// 연속된 N까지의 숫자의 범위가 아니라 배열에 주어진 N개 중에 소수인지 구분하는 것이기 떄문에 
+// 에라토스테네스체 방식으로 구현하는 방식이 아닌 것 같다. 
 
 const input = require('fs').readFileSync(path).toString().trim().split('\n')
+const arr = input[1].split(' ').map(Number);
 
 const isPrime = (n) => {
     if (n === 1) return false;
@@ -15,7 +20,5 @@ const isPrime = (n) => {
 
     return true;
 }
-
-const arr = input[1].split(' ').map(Number);
 
 console.log(arr.filter(v => isPrime(v)).length);
