542495e3dae1776d4ebab8188e6877595aa073cc
MA <36831218+rmaomina@users.noreply.github.com>
2022-10-23 00:46:20 +0900

    [Bronze V] Title: A+B - 9, Time: 148 ms, Memory: 9376 KB -BaekjoonHub

---
+++ 백준/Bronze/15740. A＋B － 9/A＋B － 9.js
@@ -0,0 +1,7 @@
+// const path = __dirname + '/input_boj1.txt'
+const path = '/dev/stdin'
+
+const input = require('fs').readFileSync(path).toString().trim().split(' ')
+const num1 = BigInt(input[0])
+const num2 = BigInt(input[1])
+console.log(String(num1 + num2))
