7e51f5f20fd181f40423e7ac2617aba013b7963a
MA <36831218+rmaomina@users.noreply.github.com>
2022-10-23 04:22:30 +0900

    [Bronze IV] Title: 세수정렬, Time: 124 ms, Memory: 9592 KB -BaekjoonHub

---
+++ 백준/Bronze/2752. 세수정렬/세수정렬.js
@@ -0,0 +1,7 @@
+// const path = __dirname + '/input_boj1.txt'
+const path = '/dev/stdin'
+
+const input = require('fs').readFileSync(path).toString().trim().split(' ').map(Number)
+
+input.sort((a, b) => a - b)
+console.log(input.join(' '))
