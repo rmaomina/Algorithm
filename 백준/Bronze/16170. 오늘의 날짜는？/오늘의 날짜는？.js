c48e5abb92dbffc8a4a80471359451df2f0a573b
MA <36831218+rmaomina@users.noreply.github.com>
2022-10-22 21:11:02 +0900

    [Bronze V] Title: 오늘의 날짜는?, Time: 112 ms, Memory: 9268 KB -BaekjoonHub

--- 백준/Bronze/16170. 오늘의 날짜는？/오늘의 날짜는？.js
+++ 백준/Bronze/16170. 오늘의 날짜는？/오늘의 날짜는？.js
@@ -1,6 +1,7 @@
-const answer = `\
-2022
-10
-22
-`
-console.log(answer)
+
+let today = new Date()
+
+const check = (day) => {
+  return day.toString().length < 2 ? `0${day}` : day
+}
+console.log(`${today.getUTCFullYear()}\n${check(today.getUTCMonth() + 1)}\n${check(today.getUTCDate())}`)
