36d49a4d7c3e44c5bcb19ac2e8ee685385af6658
MA <36831218+rmaomina@users.noreply.github.com>
2022-10-23 04:11:22 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 136 ms, Memory: 9360 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.js
@@ -0,0 +1,12 @@
+// const path = __dirname + '/input_boj1.txt'
+const path = '/dev/stdin'
+
+const input = require('fs').readFileSync(path).toString().trim()
+let result = ''
+
+for (let i = +input; i > 0; i--) {
+  result += '*'.repeat(i)
+  result += '\n'
+}
+
+console.log(result)
