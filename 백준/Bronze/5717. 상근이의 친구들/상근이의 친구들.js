3df5edbd05c7fe0b100dd30e86ffd9ec4650588a
MA <36831218+rmaomina@users.noreply.github.com>
2022-10-23 13:11:17 +0900

    [Bronze IV] Title: 상근이의 친구들, Time: 124 ms, Memory: 9312 KB -BaekjoonHub

---
+++ 백준/Bronze/5717. 상근이의 친구들/상근이의 친구들.js
@@ -0,0 +1,10 @@
+// const path = __dirname + '/input_boj1.txt'
+const path = '/dev/stdin'
+
+const input = require('fs').readFileSync(path).toString().trim().split('\n')
+const result = input
+	.slice(0, input.length - 1)
+	.map((el) => el.split(' ').reduce((a, c) => +a + +c))
+	.join('\n')
+
+console.log(result)
