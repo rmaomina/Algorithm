dfcb402fc9ee0fb050449dba5fa69f8d28cc5e51
mina <rmaomina@gmail.com>
2022-10-24 04:12:22 +0900

    백준 '인공지능 시계' 오답 추가

--- 백준/Bronze/2530. 인공지능 시계/인공지능 시계.js
+++ 백준/Bronze/2530. 인공지능 시계/인공지능 시계.js
@@ -12,3 +12,18 @@ h=h+parseInt(m/60)
 m%=60
 h%=24
 console.log("%d %d %d",h,m,sum)
+
+// 찾아본 레퍼로는 테스트가 통과가 안되는데 ;; 
+// 아래 내 코드는 테케는 통과하지만 채점이 틀렸다고 나옴 
+
+// const input = require('fs').readFileSync(path).toString().trim().split('\n')
+// const [h, m, s] = input[0].split(' ').map(BigInt)
+// const totalSeconds = BigInt(input[1]) + (h * 3600n + m * 60n + s)
+
+// const seconds = totalSeconds % 60n
+// const minutes = (totalSeconds - seconds) / 60n % 60n
+// let hours = (totalSeconds - minutes * 60n - seconds) / 3600n
+
+// if (hours > 23n) hours = 0n
+
+// console.log(`${String(hours)} ${String(minutes)} ${String(seconds)}`)
