38c084d733d2bc2a197446edeff8859385afd4dc
MA <36831218+rmaomina@users.noreply.github.com>
2022-12-11 19:04:23 +0900

    [Bronze IV] Title: 카드 뽑기, Time: 144 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/16204. 카드 뽑기/카드 뽑기.js
@@ -0,0 +1,7 @@
+// const path = __dirname + '/input_boj2.txt'
+const path = '/dev/stdin'
+
+const input = require('fs').readFileSync(path).toString().trim().split(' ')
+const [N, M, K] = input.map(Number)
+
+console.log(N - Math.abs(M - K))
