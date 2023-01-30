dbff1688f5c97489cc17caa1a8f587aa32c5d1c5
Mina Kim <rmaomina@gmail.com>
2023-01-30 20:32:26 +0900

    delete LeetCode test

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++
@@ -1,9 +0,0 @@
-/**
- * @param {number[]} nums
- * @return {boolean}
- */
- 
-var containsDuplicate = function(nums) {
-    let array = new Set(nums)
-    return nums.length !== array.size
-};
--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
+++
@@ -1,9 +0,0 @@
-/**
- * @param {number[]} nums
- * @return {number[]}
- */
-var runningSum = function(nums) {
-    return nums.map((el, idx) => {
-      return nums.slice(0, idx + 1).reduce((acc, cur) => acc + cur)
-    })
-};
