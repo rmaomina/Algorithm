6a877330e1ea1070b48a8a5920da8a463edc59ce
MA <36831218+rmaomina@users.noreply.github.com>
2023-04-06 21:38:07 +0900

    [Bronze IV] Title: SMS from MCHS, Time: 128 ms, Memory: 9332 KB -BaekjoonHub

---
+++ 백준/Bronze/21638. SMS from MCHS/SMS from MCHS.js
@@ -0,0 +1,20 @@
+const path = '/dev/stdin'
+
+let input = require('fs')
+.readFileSync(path)
+.toString()
+.trim()
+.split("\n")
+
+const [t1, v1] = input[0].split(" ").map(Number)
+const [t2, v2] = input[1].split(" ").map(Number)
+
+if (t2 < 0 && v2 >= 10) {
+  console.log("A storm warning for tomorrow! Be careful and stay home if possible!")
+} else if (t1 > t2) {
+  console.log("MCHS warns! Low temperature is expected tomorrow.")
+} else if (v1 < v2) {
+  console.log("MCHS warns! Strong wind is expected tomorrow.")
+} else {
+  console.log("No message")
+}
