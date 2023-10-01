ba5231c90bd8a87ee0a848893b2353ad2db0a25f
Mina <36831218+rmaomina@users.noreply.github.com>
2023-10-01 15:09:15 +0900

    [Bronze V] Title: 스위트콘 가격 구하기, Time: 128 ms, Memory: 9580 KB -BaekjoonHub

---
+++ 백준/Bronze/30030. 스위트콘 가격 구하기/스위트콘 가격 구하기.js
@@ -0,0 +1,10 @@
+
+// const path = __dirname + '/input_boj5.txt'
+const path = '/dev/stdin'
+
+let input = require('fs')
+.readFileSync(path)
+.toString()
+.trim()
+
+console.log(+input / 110 * 100);
